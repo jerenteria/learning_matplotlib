76a6d210a0e6d0b069d503de8387464e1e71532f
jerenteria <renteria.juan96@gmail.com>
2022-12-29 20:56:22 -0800

    added some speudocode too tired

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,8 +1,11 @@
+# Import so we dont have to keep typing pyplot repeatedly
 import matplotlib.pyplot as plt
 
+# List that will be plotted
 squares = [1,4,9,16,25]
-
-fi, ax = plt.subplots()
+# fig = entire figure or collection of plots that are generated, ax = single plot in the figure 
+# subplots() can generate one or more plots in the same figure
+fig, ax = plt.subplots()
 ax.plot(squares)
 
 plt.show()
