a4972953573c586d1bda9d7c9293573202fe291b
jerenteria <renteria.juan96@gmail.com>
2022-12-30 21:54:18 -0800

    matplotplib

--- mpl_squares.py
+++ mpl_squares.py
@@ -6,6 +6,6 @@ squares = [1,4,9,16,25]
 # fig = entire figure or collection of plots that are generated, ax = single plot in the figure 
 # subplots() can generate one or more plots in the same figure
 fig, ax = plt.subplots()
-ax.plot(squares)
+ax.plot(squares, linewidth=3)
 
 plt.show()
